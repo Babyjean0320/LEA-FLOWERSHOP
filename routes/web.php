29359b43da0df09035340dbd8bbdbcca18277819
Babyjean0320 <Coronelbabyjean@gmail.com>
2023-01-27 13:03:23 +0800

    Edited users in dashboard added functionality CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UsersController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\SliderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,7 +24,23 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/users', [UsersController::class, 'index'])->middleware(['auth', 'verified'])->name('users');
+Route::get('/users', [UserController::class, 'index'])
+        ->middleware(['auth', 'verified'])
+        ->name('users');
+
+Route::get('/users/add', [UserController::class, 'form'])
+        ->middleware(['auth', 'verified']);
+Route::post('/users/add', [UserController::class, 'store'])
+        ->middleware(['auth', 'verified']);
+
+Route::get('/users/update/{id}', [UserController::class, 'show'])
+        ->middleware(['auth', 'verified']);
+Route::post('/users/update/{id}', [UserController::class, 'update'])
+        ->middleware(['auth', 'verified']);
+
+Route::get('/users/delete/{id}', [UserController::class, 'delete'])
+        ->middleware(['auth', 'verified']);     
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

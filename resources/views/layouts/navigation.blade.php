06d849fc1589d0938545b106c60c5fe672f2a35e
Babyjean0320 <Coronelbabyjean@gmail.com>
2023-01-31 22:17:48 +0800

    Update Color in Dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,12 +1,12 @@
-<nav x-data="{ open: false }" class="bg-gray-700">
+<nav x-data="{ open: false }" class="bg-gray-300">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-700">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-gray-300">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-black-800" />
                     </a>
                 </div>
 
